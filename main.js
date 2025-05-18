41cd822afea15d79f4a03b2a93306f286902b81a
GabRioBlu <parent_gabriel@hotmail.com>
2025-05-18 19:01:12 -0400

    upgrade!!

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 var clicks = 0;
+var clicksPerClick = 1;
+var upgradeCost = 25;
 
 function clickIcon() {
-    clicks++;
+    clicks += clicksPerClick;
     document.getElementById("clickCounter").innerText = "Clicks: " + clicks;
+}
+
+function buyUpgrade(){
+    if (clicks < upgradeCost) return;
+    clicks -= upgradeCost;
+    upgradeCost *= 2;
+    document.getElementById("costMarker").innerText = "cost: " + upgradeCost + " clicks";
+    document.getElementById("clickCounter").innerText = "Clicks: " + clicks;
+    clicksPerClick++;
 }
